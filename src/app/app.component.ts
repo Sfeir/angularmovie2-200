1dde17054d58c83554aa0da15e6c6db530d897ad
Wassim CHEGHAM <github@wassimchegham.com>
2016-07-18 15:47:21 +0200

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  name: string = 'MediaMétrie';
-}
+export class AppComponent {}
