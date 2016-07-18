5725a6a4997a0a40692728db85ff268bb604b728
Wassim CHEGHAM <github@wassimchegham.com>
2016-07-18 17:17:18 +0200

    solution

--- src/app/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 import {AddMovieComponent} from '../add-movie/';
 
 @Component({
@@ -17,50 +20,28 @@ export class MoviesListComponent implements OnInit {
   // access local child Component
   @ViewChild(AddMovieComponent) modal: AddMovieComponent;
 
-  constructor() { }
+  constructor(
+    private http: Http
+  ) { }
 
   ngOnInit() {
-    this.movies = [{
-      "id": 1,
-      "title": "Avatar",
-      "releaseYear": "2010",
-      "poster": "img/avatar.jpg",
-      "directors": "James Cameron",
-      "actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang, Michelle Rodriguez",
-      "synopsis": "Sur la lointaine planète de Pandora, Jake Sully, un héros malgré lui, se lance dans une quête de rédemption, de découverte, d'amour inattendu, dont l'issue sera un combat héroïque pour sauver toute une civilisation.",
-      "rate": "3"
-    }, {
-        "id": 2,
-        "title": "Seigneur des Anneaux : La Communauté de l'Anneau",
-        "releaseYear": "2003",
-        "poster": "img/seigneurdesanneaux1.jpg",
-        "directors": "Peter Jackson",
-        "actors": "Elijah Wood, Sean Astin, Ian McKellen, Sala Baker, Viggo Mortensen",
-        "synopsis": "Frodon le Hobbit hérite de l'Anneau Unique, un instrument de pouvoir absoluqui permettrait à Sauron, le Seigneur des ténèbres, de régner sur la Terre du Milieu. Commence alors un vaste périple visant à la destruction de l'objet.",
-        "rate": "5"
-      }, {
-        "id": 3,
-        "title": "The Grudge",
-        "releaseYear": "2004",
-        "poster": "img/thegrudge.jpg",
-        "directors": "Takashi Shimizu",
-        "actors": "Sarah Michelle Gellar, Jason Behr, Clea DuVall, Kadee Strickland, Bill Pullman",
-        "synopsis": "Dans ce qui paraît être une paisible maison de Tokyo se cache un épouvantable fléau. Quiconque franchit le seuil de la demeure est aussitôt frappé par une malédiction qui ne tardera pas à le tuer dans un sentiment d'indicible rage...",
-        "rate": "4"
-      }, {
-        "id": 4,
-        "title": "Yip Man 2",
-        "releaseYear": "2010",
-        "poster": "img/yipman.jpg",
-        "directors": "Wilson Yip",
-        "actors": "Donnie Yen, Sammo Hung Kam-Bo, Simon Yam, Lynn Hung, Xiaoming Huang",
-        "synopsis": "Film biographique sur la vie de Ip Man, pionnier du Wing Chun et maitre de Bruce Lee.",
-        "rate": "5"
-      }];
+    this.movies = [];
+    this.getMovies();
+  }
+
+  getMovies() {
+    this.http.get('http://localhost:9000/api/movies').map(res => res.json())
+      .subscribe((movies) => {
+        this.movies = movies;
+      });
   }
 
   addMovie(movie) {
-    this.movies.push(movie);
+    this.http.post('http://localhost:9000/api/movies', JSON.stringify(movie), { headers: new Headers({ 'Content-Type': 'application/json' }) })
+      .map(res => res.json())
+      .subscribe((newMovie) => {
+        this.movies.push(newMovie);
+      });
   }
 
   showModal() {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
+import {HTTP_PROVIDERS} from '@angular/http';
 
 import { AppComponent } from './app/';
 
 bootstrap(AppComponent, [
   disableDeprecatedForms(),
-  provideForms()
+  provideForms(),
+  HTTP_PROVIDERS
 ]);
