01057f4c464c1136b61e8293960106914ecd7c60
Wassim CHEGHAM <github@wassimchegham.com>
2016-07-18 15:11:20 +0200

    step-00

--- src/app/shared/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './highlight.directive';
-export * from './lazy.directive';
-export * from './rate.pipe';
-export * from './movies.service';
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,1 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { provide } from '@angular/core';
-import { HTTP_PROVIDERS } from '@angular/http';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import {disableDeprecatedForms, provideForms} from '@angular/forms';
-
-import { AppComponent, environment } from './app/';
-import { APP_ROUTER_PROVIDERS } from './app/app.routes';
-
-bootstrap(AppComponent, [
-  HTTP_PROVIDERS,
-  APP_ROUTER_PROVIDERS,
-  disableDeprecatedForms(),
-  provideForms(),
-  { provide: LocationStrategy, useClass: HashLocationStrategy }
-]);
